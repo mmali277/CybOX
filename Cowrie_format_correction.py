c548cc6acc32a513bd865e615a168e0c53de9555
mmali277 <mmali277@gmail.com>
2018-05-15 14:11:55 +0500

    Update Cowrie_format_correction.py

--- Cowrie_format_correction.py
+++ Cowrie_format_correction.py
@@ -1,6 +1,7 @@
-#cowrie_file = open('G:\Cyber Security\cybox and stix\logs\cowrie.json', mode='at', encoding='utf-8')
 
-f = open('C:\\Users\DELL\Desktop\cowrie mam logs.json','r+')
+
+#your file path comes here:
+f = open('C:\\Users\DELL\Desktop\cowrie_logs.json','r+')
 lines = f.readlines() # read old content
 f.seek(0) # go back to the beginning of the file
 f.write('[') # write new content at the beginning
@@ -14,7 +15,3 @@ for line in lines: # write old content after new
 f.write(']')
 f.close()
 
-#cowrie_file.seek(0)
-#cowrie_file.write('[')
-#cowrie_file.close()
-#print(cowrie_file.read())
